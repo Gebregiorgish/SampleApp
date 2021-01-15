363fe9dca8700c75ffdc55b2d0000d38f89de7e1
Haimi Geberegiorgis <haimi.gebregiorgis@gmail.com>
2021-01-14 19:39:35 -0500

    Test_Employees_Added

--- Program.cs
+++ Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace SampleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            List<Employee> employees = new List<Employee>()
+        {
+             new Employee() { Id=1, FirstName="Sam", LastName="Adams", Salary=75000},
+             new Employee() { Id=2, FirstName="John", LastName="Doh", Salary=85000},
+             new Employee() { Id=3, FirstName="Justin",LastName="Jason", Salary=100000},
+             new Employee() { Id=4, FirstName="Selena", LastName="Trump", Salary=120000},
+             new Employee() { Id=5, FirstName="Ruth", LastName="Nathen", Salary=150000},
+
+
+        };
+           foreach (var e in employees)
+            {
+                
+               Console.WriteLine( e.PrintFristNameLastName(e));
+                Console.WriteLine("----------------------------");
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Salary { get; set; }
+
+        public  string PrintFristNameLastName(Employee employee)
+        {
+            //Employee e = new Employee();
+
+             return employee.FirstName + " " + employee.FirstName;
         }
     }
 }
